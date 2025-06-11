299d40d9b7994f2af9f7f634594902890327e48f
Eugene-ak <amedioreugene@gmail.com>
2025-06-11 17:30:24 +0000

    Refactor password generation logic and move utility functions to a separate file

--- src/components/Cards/AppCard.tsx
+++ src/components/Cards/AppCard.tsx
@@ -8,70 +8,9 @@ import Checkbox from "../Inputs/Checkbox";
 import { BottomSectionDiv } from "../Sections/BottomSection.style";
 import PasswordLength from "../Sections/PasswordLength";
 import StrengthContainer from "../Sections/StrengthContainer";
+import { generatePassword } from "../../../utils/functions";
 
 export default function AppCard() {
-  const uppercaseLetters: string[] = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-
-  const lowercaseLetters: string[] = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-
-  const numbers: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-  const symbols: string[] = ["!", "@", "#", "$", "%", "&", "*", "(", ")"];
-
   const range: MutableRefObject<HTMLInputElement | null> = useRef(null);
   const uppercaseCheck: MutableRefObject<HTMLInputElement | null> =
     useRef(null);
@@ -87,7 +26,7 @@ export default function AppCard() {
   const updateRangeValue = () => {
     let value: number = 0;
     if (range.current?.value) {
-    value = parseInt(range.current?.value);
+      value = parseInt(range.current?.value);
     }
     setRangeValue(value);
   };
@@ -100,60 +39,6 @@ export default function AppCard() {
       setCheckedStateCount((prevState) => prevState + 1);
     } else {
       setCheckedStateCount((prevState) => prevState - 1);
-    }
-  };
-
-  // Shuffle the array
-  function shuffleArray(array: string[]): string[] {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-    }
-    return array;
-  }
-
-  // Generate password per conditions specified
-  const generatePassword = () => {
-    if (rangeValue !== undefined) {
-      const passLength: number = rangeValue;
-      const newArray = [];
-
-      // Append items to array
-      for (let i = 1; i <= passLength; i++) {
-        if (uppercaseCheck.current?.checked) {
-          newArray.push(
-            uppercaseLetters[
-              Math.floor(Math.random() * uppercaseLetters.length)
-            ]
-          );
-        }
-
-        if (lowercaseCheck.current?.checked) {
-          newArray.push(
-            lowercaseLetters[
-              Math.floor(Math.random() * lowercaseLetters.length)
-            ]
-          );
-        }
-
-        if (numbersCheck.current?.checked) {
-          newArray.push(numbers[Math.floor(Math.random() * numbers.length)]);
-        }
-
-        if (symbolsCheck.current?.checked) {
-          newArray.push(symbols[Math.floor(Math.random() * symbols.length)]);
-        }
-      }
-
-      // Convert new password array to string
-      let newPassword = shuffleArray(newArray).join("");
-
-      // Slice the password to be equal to the range specified
-      if (newPassword.length > passLength) {
-        newPassword = newPassword.slice(0, passLength).toString();
-        setPassword(newPassword);
-      }
-      setPassword(newPassword);
     }
   };
 
@@ -198,7 +83,18 @@ export default function AppCard() {
           text="Symbols"
         />
         <StrengthContainer count={checkedStateCount} rangeValue={rangeValue} />
-        <GenerateButton onClick={generatePassword} />
+        <GenerateButton
+          onClick={() =>
+            generatePassword(
+              rangeValue,
+              uppercaseCheck,
+              lowercaseCheck,
+              numbersCheck,
+              symbolsCheck,
+              setPassword
+            )
+          }
+        />
       </BottomSectionDiv>
     </StyledCard>
   );
--- utils/functions.ts
+++ utils/functions.ts
@@ -1,17 +1,165 @@
 import { MutableRefObject } from "react";
 import { toast } from "sonner";
 
-export const copyText = (text: string, copiedText: MutableRefObject<HTMLSpanElement | null>) => {
-    // Check if the password field has a value other than default and make sure reference to the copy button is undefined
-    if (text && copiedText?.current) {
-      navigator.clipboard.writeText(text);
-      // Display copied text for 2seconds and revert to default state on successful copy
-      copiedText.current.style.display = "flex";
-      toast.success("Copied to clipboard!");
-      setTimeout(() => {
-        if (copiedText.current) {
-          copiedText.current.style.display = "none";
-        }
-      }, 2000);
+export const uppercaseLetters: string[] = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+export const lowercaseLetters: string[] = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+export const numbers: string[] = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+
+export const symbols: string[] = ["!", "@", "#", "$", "%", "&", "*", "(", ")"];
+
+export const copyText = (
+  text: string,
+  copiedText: MutableRefObject<HTMLSpanElement | null>
+) => {
+  // Check if the password field has a value other than default and make sure reference to the copy button is undefined
+  if (text && copiedText?.current) {
+    navigator.clipboard.writeText(text);
+    // Display copied text for 2seconds and revert to default state on successful copy
+    copiedText.current.style.display = "flex";
+    toast.success("Copied to clipboard!");
+    setTimeout(() => {
+      if (copiedText.current) {
+        copiedText.current.style.display = "none";
+      }
+    }, 2000);
+  }
+};
+
+// Shuffle the array
+export function shuffleArray(array: string[]): string[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+  }
+  return array;
+}
+
+// Generate password per conditions specified
+export const generatePassword = (
+  rangeValue: number,
+  uppercaseCheck: MutableRefObject<HTMLInputElement | null>,
+  lowercaseCheck: MutableRefObject<HTMLInputElement | null>,
+  numbersCheck: MutableRefObject<HTMLInputElement | null>,
+  symbolsCheck: MutableRefObject<HTMLInputElement | null>,
+  setPassword: (password: string) => void
+) => {
+  if (rangeValue < 4) {
+    toast.info("Please select a password length of at least 4 characters.");
+    return;
+  }
+
+  if (
+    !uppercaseCheck.current?.checked &&
+    !lowercaseCheck.current?.checked &&
+    !numbersCheck.current?.checked &&
+    !symbolsCheck.current?.checked
+  ) {
+    toast.info("Please select at least one character type.");
+    return;
+  }
+
+  if (rangeValue !== undefined) {
+    const passLength: number = rangeValue;
+    const newArray = [];
+
+    // Append items to array
+    for (let i = 1; i <= passLength; i++) {
+      if (uppercaseCheck.current?.checked) {
+        newArray.push(
+          uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)]
+        );
+      }
+
+      if (lowercaseCheck.current?.checked) {
+        newArray.push(
+          lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]
+        );
+      }
+
+      if (numbersCheck.current?.checked) {
+        newArray.push(numbers[Math.floor(Math.random() * numbers.length)]);
+      }
+
+      if (symbolsCheck.current?.checked) {
+        newArray.push(symbols[Math.floor(Math.random() * symbols.length)]);
+      }
     }
-  };
+
+    // Convert new password array to string
+    let newPassword = shuffleArray(newArray).join("");
+
+    // Slice the password to be equal to the range specified
+    if (newPassword.length > passLength) {
+      newPassword = newPassword.slice(0, passLength).toString();
+      setPassword(newPassword);
+    }
+    setPassword(newPassword);
+  }
+};
